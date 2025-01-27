547138d9c749cf235ff24a6f6b23bf91eb7c1dd1
Korytin Nikita <korytin@spcube.ru>
2025-01-27 09:53:49 +0500

    Add project files.

---
+++ Toolkit.Debouncing/Abstractions/IDebounceDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace Toolkit.Debouncing.Abstractions
+{
+    internal interface IDebounceDispatcher
+    {
+        void Debounce(int interval, Action<object> action,
+            object param = null,
+            DispatcherPriority priority = DispatcherPriority.ApplicationIdle,
+            Dispatcher disp = null);
+
+        void Throttle(int interval, Action<object> action,
+            object param = null,
+            DispatcherPriority priority = DispatcherPriority.ApplicationIdle,
+            Dispatcher disp = null);
+    }
+}
---
+++ Toolkit.Debouncing/DebounceDispatcher.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Threading;
+using System;
+using Toolkit.Debouncing.Abstractions;
+
+namespace Toolkit.Debouncing
+{
+    /// <summary>
+    /// Provides Debounce() and Throttle() methods.
+    /// Use these methods to ensure that events aren't handled too frequently.
+    /// 
+    /// Throttle() ensures that events are throttled by the interval specified.
+    /// Only the last event in the interval sequence of events fires.
+    /// 
+    /// Debounce() fires an event only after the specified interval has passed
+    /// in which no other pending event has fired. Only the last event in the
+    /// sequence is fired.
+    /// </summary>
+    internal class DebounceDispatcher : IDebounceDispatcher
+    {
+        private DispatcherTimer timer;
+        private DateTime timerStarted { get; set; } = DateTime.UtcNow.AddYears(-1);
+
+        public void Debounce(int interval, Action<object> action,
+            object param = null,
+            DispatcherPriority priority = DispatcherPriority.ApplicationIdle,
+            Dispatcher disp = null)
+        {
+            // kill pending timer and pending ticks
+            timer?.Stop();
+            timer = null;
+
+            if (disp == null)
+                disp = Dispatcher.CurrentDispatcher;
+
+            // timer is recreated for each event and effectively
+            // resets the timeout. Action only fires after timeout has fully
+            // elapsed without other events firing in between
+            timer = new DispatcherTimer(TimeSpan.FromMilliseconds(interval), priority, (s, e) =>
+            {
+                if (timer == null)
+                    return;
+
+                timer?.Stop();
+                timer = null;
+                action.Invoke(param);
+            }, disp);
+
+            timer.Start();
+        }
+
+        public void Throttle(int interval, Action<object> action,
+            object param = null,
+            DispatcherPriority priority = DispatcherPriority.ApplicationIdle,
+            Dispatcher disp = null)
+        {
+            // kill pending timer and pending ticks
+            timer?.Stop();
+            timer = null;
+
+            if (disp == null)
+                disp = Dispatcher.CurrentDispatcher;
+
+            var curTime = DateTime.UtcNow;
+
+            // if timeout is not up yet - adjust timeout to fire 
+            // with potentially new Action parameters           
+            if (curTime.Subtract(timerStarted).TotalMilliseconds < interval)
+                interval -= (int)curTime.Subtract(timerStarted).TotalMilliseconds;
+
+            timer = new DispatcherTimer(TimeSpan.FromMilliseconds(interval), priority, (s, e) =>
+            {
+                if (timer == null)
+                    return;
+
+                timer?.Stop();
+                timer = null;
+                action.Invoke(param);
+            }, disp);
+
+            timer.Start();
+            timerStarted = curTime;
+        }
+    }
+}
