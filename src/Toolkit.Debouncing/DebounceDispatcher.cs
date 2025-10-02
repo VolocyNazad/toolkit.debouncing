0439b1746b980d8fa9559d6fd095665e22585814
Korytin Nikita <korytin.nikita@gmail.com>
2025-10-02 20:41:37 +0500

    Добавление перегрузок для DebounceDispatcher.

--- src/Toolkit.Debouncing/DebounceDispatcher.cs
+++ src/Toolkit.Debouncing/DebounceDispatcher.cs
@@ -23,6 +23,32 @@ namespace Toolkit.Debouncing
         private DispatcherTimer? timer;
         private DateTime lastExecution = DateTime.UtcNow.AddYears(-1);
 
+        public void Debounce(int interval, Action action,
+            DispatcherPriority priority = DispatcherPriority.Normal,
+            Dispatcher? dispatcher = default)
+        {
+            using (_lock.EnterScope())
+            {
+                timer?.Stop();
+
+                dispatcher ??= Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+
+                if (dispatcher.CheckAccess() && !dispatcher.HasShutdownStarted)
+                    return;
+
+                timer = new(TimeSpan.FromMilliseconds(interval), priority, (s, e) =>
+                {
+                    using (_lock.EnterScope())
+                    {
+                        timer?.Stop();
+                    }
+                    action.Invoke();
+                }, dispatcher);
+
+                timer.Start();
+            }
+        }
+
         public void Debounce<TParameter>(int interval, Action<TParameter?> action,
             TParameter? parameter = default,
             DispatcherPriority priority = DispatcherPriority.Normal,
@@ -44,6 +70,47 @@ namespace Toolkit.Debouncing
                         timer?.Stop();
                     }
                     action.Invoke(parameter);
+                }, dispatcher);
+
+                timer.Start();
+            }
+        }
+
+        public void Throttle(int interval, Action action,
+            DispatcherPriority priority = DispatcherPriority.Normal,
+            Dispatcher? dispatcher = default)
+        {
+            using (_lock.EnterScope())
+            {
+                timer?.Stop();
+
+                dispatcher ??= Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+
+                if (dispatcher.CheckAccess() && !dispatcher.HasShutdownStarted)
+                    return;
+
+                var currentTime = DateTime.UtcNow;
+                var timeSinceLast = currentTime - lastExecution;
+
+                if (timeSinceLast.TotalMilliseconds < interval)
+                {
+                    interval -= (int)timeSinceLast.TotalMilliseconds;
+                }
+                else
+                {
+                    action.Invoke();
+                    lastExecution = currentTime;
+                    return;
+                }
+
+                timer = new(TimeSpan.FromMilliseconds(interval), priority, (s, e) =>
+                {
+                    using (_lock.EnterScope())
+                    {
+                        timer?.Stop();
+                        lastExecution = DateTime.UtcNow;
+                    }
+                    action.Invoke();
                 }, dispatcher);
 
                 timer.Start();
